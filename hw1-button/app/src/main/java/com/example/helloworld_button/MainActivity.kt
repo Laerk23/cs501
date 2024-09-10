a2bbd75cd3200c008bd0fa61d9bc83610cfa916b
Laerk Ndreko <laerkn23@bu.edu>
2024-09-10 15:38:15 -0400

    created hw1 for hello world initial file, and hw1-button for file with button to display toast message containing hello world

---
+++ hw1-button/app/src/main/java/com/example/helloworld_button/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.kotlinbuttonclick
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Greeting("Hello World!")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Hello $name!", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Click me")
+    }
+}
